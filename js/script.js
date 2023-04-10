dca2effde8d529a50703e57f465fc21cc7eafb20
Vitorrrrrrrrr <vitoravs008@gmail.com>
2023-04-10 08:57:36 -0300

    WEEEEEEEE

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
 function regBox(){
-     let text = document.getElementsByClassName("banner");
-     let box = '<div class="leleco"> <h2>Parabéns!</h2> <span>Você foi registrado(a) em nosso Newsletter</span></div>';
-
-     text.innerHTML = box;
+   alert("Parabéns! Você está registrado em nosso Newsletter")
 }

313e85897f14c69a0760436ae0fc43b5f20371c5
Vitor Antonio Vieira da Silva <125030836+VitorAntonioVieira@users.noreply.github.com>
2023-04-10 02:04:11 -0300

    gbweufb

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 function regBox(){
-    
+     let text = document.getElementsByClassName("banner");
+     let box = '<div class="leleco"> <h2>Parabéns!</h2> <span>Você foi registrado(a) em nosso Newsletter</span></div>';
+
+     text.innerHTML = box;
 }
